297d9d660b8aa542e27c472f8ec69e808eb8a293
LadiesMan217 <88813483+elvinsanity98@users.noreply.github.com>
2024-09-04 01:09:02 +0800

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -21,12 +21,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
         // Server settings
         $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'smtp-relay.brevo.com';                     // Set the SMTP server to send through
+        $mail->Host       = 'smtp.gmail.com';                     // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = '7b656a002@smtp-brevo.com';               // SMTP username
-        $mail->Password   = 'COaQbEfNAmkMd9Ij';                  // SMTP password
+        $mail->Username   = 'kaeltheinvoker121@gmail.com';               // SMTP username
+        $mail->Password   = 'vdqtroowilupbcdk';                  // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-        $mail->Port       = 587;                                    // TCP port to connect to
+        $mail->Port       = 465;                                    // TCP port to connect to
 
         // Recipients
         $mail->setFrom('7b656a002@smtp-brevo.com', 'Your Name');
